d363c668d88d39d3ca7f77e61784d1878e6445ff
martinapasquon <116556465+martinapasquon@users.noreply.github.com>
2023-05-30 17:15:46 +0200

    Update main.h

--- test_prob/main.h
+++ test_prob/main.h
@@ -1,17 +1,17 @@
 
 
-#ifndef __fluid__
-#define __fluid__
+#ifndef __simulazione__
+#define __simulazione__
 
 
 
 
-const int N_0=50;
+const int N_0=50; //numero di cellule alla generazione 0
 long conta_out=0;
-const int n_gen=10;
+const int n_gen=10; //numero di generazioni
 const int n_cellule=3;
-const int n_tr_tot=50000;
-double cost=0;
+const int n_tr_tot=50000; //numero di simulazioni
+double cost=0; 
 int n=0;
 
 
@@ -19,11 +19,6 @@ double coppie_2=0;
 
 long v[n_gen];
 long double diff[n_gen];
-double p=0;
-long N_max=0;
-int N_av=0;
-long coppie=0;
-long b_root=0;
 long v_0[n_gen-1];
 long v_1[n_gen-1];
 long v_2[n_gen-1];
@@ -34,19 +29,14 @@ double P_1=2.*P_alive*(1-P_alive);
 double P[3];
 unsigned int n_figlie[3];
 
-double casuale=0;
 long int cell[n_cellule];
 long gen[n_cellule];
 double v_dead=(1-pow(1-P_alive, 2));
-long costa=0, costa_a=0, costa_b=0;
 long  a=0;
 int conta=0; 
-int conta1=0;
 
 vector <unsigned long> v_gen;
 
-//unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-//default_random_engine generator(seed);
 
 #endif
 

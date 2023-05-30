eab6c009bfb66b7d47000c4bdb2de217c15bd41b
martinapasquon <116556465+martinapasquon@users.noreply.github.com>
2023-05-30 16:47:51 +0200

    Update main.h

--- es_new/main.h
+++ es_new/main.h
@@ -4,38 +4,37 @@
 #define __fluid__
 
 //parametri esperimento
-int N_0=100;
-int n_gen=34;
-const int n_cellule=3;
-//double b=1.1886;
-//double d=0.7346;
-const int N_cell_bott=1065000;
-int n_gen_0=120;
-double mut_totali=2099;
-//int n_gen0_arr_0[23]={3350,3200,3050,2900,2750,2600,2450,2300,2150,2000,1850,1700,1550,1400,1250,1100,950,800,650,500,350,200,50};
-//int n_gen0_arr_0[26]={ 4100,3950,3800,3650,3500,3350,3200,3050,2900,2750,2600,2450,2300,2150,2000,1850,1700,1550,1400,1250,1100,950,800,650,500,350};
-//int n_gen0_arr_0[32]={ 5000,4850,4700,4550,4400,4250,4100,3950,3800,3650,3500,3350,3200,3050,2900,2750,2600,2450,2300,2150,2000,1850,1700,1550,1400,1250,1100,950,800,650,500,350};
-// usato prima int n_gen0_arr_0[33]={324,  265,  224,  196,  174,  157,  143,  131,  122,  114, 107, 101, 95, 90, 86, 82, 79, 76, 73, 70, 67, 65, 63, 61, 59, 57, 56, 55, 54, 52, 51, 50, 49};
-// usato prima int n_gen_arr[25]={49, 44, 40, 37, 35, 33, 31, 29, 28, 26, 25, 24, 23, 22, 21, 21, 20, 19, 19, 18, 18, 17, 17, 17, 16};
- int n_gen0_arr_0[33]={324,265,224,196,174,157,  143,  131,  122,  114, 107, 101, 95, 90, 86, 82, 79, 76, 73, 70, 67, 65, 63, 61, 59, 57, 56, 55, 54, 52, 51, 50, 49};
-int n_gen_arr[33]={104,84,71,61,54,49, 44, 40, 37, 35, 33, 31, 29, 28, 26, 25, 24, 23, 22, 21, 21, 20, 19, 19, 18, 18, 17, 17, 17, 16};
-//int n_gen_arr[33]={ 104,84,71,61,54,49, 44, 40, 37, 35, 33, 31, 29, 28, 26, 25, 24, 23, 22, 21, 21, 20, 19, 19, 18, 18, 17, 17, 17, 16, 16, 15, 15};
+int N_0=100; //numero di cellule dei bottlenecks
+int n_gen=34; //numero di generazioni tra due bottlenecks 
+const int n_cellule=3; //numero di cellule sequenziate
+const int N_cell_bott=1065000; //numero minimo di cellule alla generazione N_gen0
+int n_gen_0=120; //numero di generazioni tra l'inizio dell'esperimento e il primo bottleneck (N_gen0)
+double mut_totali=2099; //mutazioni totali
+//array se si vuole far variare N_gen0 (il primo valore è per P_a=0.52, il secondo per P_a=0.525 ecc, P_a varia di 0.05. L'array è ugulale per tutte le
+//linee cellulari).
+int n_gen0_arr_0[33]={324,265,224,196,174,157,  143,  131,  122,  114, 107, 101, 95, 90, 86, 82, 79, 76, 73, 70, 67, 65, 63, 61, 59, 57, 56, 55, 54, 52, 51, 50, 49};
+//array se si vuole far variare N_gen (il primo valore è per P_a=0.52, il secondo per P_a=0.525 ecc, P_a varia di 0.05 e b-d è tenuto costante. 
+//qui ad es è per la linea cellulare CRC1599LM, ma è diverso per ogni linea cellualre)
+int n_gen_arr[33]={35,36,37,37,38,39, 40, 41, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 57, 58, 59, 61, 62};
+
+
 double mu_arr[1000*5*1000];
 unsigned int n_figlie[3];
-const int n_alb=7;
+const int n_alb=7; //numero di bottlenecks a N_0
 const int n_cell=2;
-double N_m=2748711013;
-double mu=6.33828E-09;
+double N_m=2748711013; //numero di basi sequenziate
+double mu=6.33828E-09; //tasso di mutazione
 int v_i=0;
-//double numero_basi[5]={N_m,N_m,N_m,N_m,N_m};
-//double mu_rate[5]={mu,mu,mu,mu,mu};
+
 
 //simulazione
-const int n_tr_tot=1000;
+const int n_tr_tot=1000; //numero di simulazioni
 int restart;
 double uguali_N_0=0;
 int conta_N_0=0;
+
 //simulazione popolazione
+
 vector <unsigned long long> v;
 vector <unsigned long long> v_possibili;
 vector <unsigned long long> v_possibili2;
@@ -46,18 +45,18 @@ vector <unsigned long long> v_1;
 vector <unsigned long long> v_2;
 unsigned long long  gen[n_cellule];
 
-//double p=0.6;
+
 unsigned long long N_max=0;
 int a=0;
 unsigned long long  a_i=0;
 int conta=0;
 int conta1=0,  conta_gen0=0;
 unsigned long long  conta_norip=0;
-double P_alive=0.57814634;
+double P_alive=0.57814634; //P_a 
 double v_dead=(1-pow(1-P_alive, 2));
-double P_2=P_alive*P_alive;
-double P_0=(pow(1-P_alive, 2));
-double P_1=2.*P_alive*(1-P_alive);
+double P_2=P_alive*P_alive; //probabilità di avere due figlie
+double P_0=(pow(1-P_alive, 2)); //probabilità di non avere figlie
+double P_1=2.*P_alive*(1-P_alive);  //probabilità di avere una figlia
 unsigned long long  costa=0, costa_a=0, costa_b=0, conta_probablilita=0;
 double P[3];
  
@@ -72,27 +71,11 @@ int errore=0;
 
 //simulazione mutazioni
 int rami[5];
-int tau[n_cellule];
-int ar, br, cr, dr;
-//int rami_piccoli[n_cellule+(n_cellule-1)];
 int mutazioni[n_cellule+ (n_cellule-1)];
 int mutazioni_tot=0;
 int N_iter=0;
 int i_ramo=0;
 int mut_tot=0;
-//ouble mu_arr[n_cellule+ (n_cellule-1)];
-double mu_2[n_cellule+ (n_cellule-1)];
-
-double mu_av[n_cellule+ (n_cellule-1)];
-double mu2_av[n_cellule+ (n_cellule-1)];
-double stdev_mu[n_cellule+ (n_cellule-1)];
-
-vector <int> tipo_mut_0;
-vector <int> tipo_mut_1;
-vector <int> tipo_mut_2;
-vector <int> tipo_mut_3;
-vector <int> tipo_mut_4;
-vector <int> tipo_mut_tot;
 
 unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 default_random_engine generator(seed);
